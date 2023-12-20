f1768c63b369511f4934b1c86f25dd5e3cac2dc4
curarin <145868208+curarin@users.noreply.github.com>
2023-12-20 14:27:57 +0100

    Remove Login wall
    loading issues due to login wall. removed

--- app.py
+++ app.py
@@ -17,46 +17,7 @@ with st.sidebar:
     sidebar.sidebar()
 
 ########################################################################
-########################################################################
-########################################################################
-def check_password():
-    """Returns `True` if the user had a correct password."""
 
-    def login_form():
-        """Form with widgets to collect user information"""
-        with st.form("Credentials"):
-            st.text_input("Username", key="username")
-            st.text_input("Password", type="password", key="password")
-            st.form_submit_button("Log in", on_click=password_entered)
-
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["username"] in st.secrets[
-            "passwords"
-        ] and hmac.compare_digest(
-            st.session_state["password"],
-            st.secrets.passwords[st.session_state["username"]],
-        ):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the username or password.
-            del st.session_state["username"]
-        else:
-            st.session_state["password_correct"] = False
-
-    # Return True if the username + password is validated.
-    if st.session_state.get("password_correct", False):
-        return True
-
-    # Show inputs for username + password.
-    login_form()
-    if "password_correct" in st.session_state:
-        st.error("😕 User not known or password incorrect")
-    return False
-if not check_password():
-    st.stop()
-########################################################################
-########################################################################
-########################################################################
 # Main Streamlit app starts here
 tab1, tab2 = st.tabs([
     "Google & Bing IndexNOW |",
